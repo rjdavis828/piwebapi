b05a3c009a8ef92e0e234f3826478523b8b4d5a5
Ryan Davis <rjdavis828@gmail.com>
2025-05-22 21:12:22 +0200

    added more examples

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#This is an example of how to use the PI Web API to pull data from a PI server
+
 import pandas as pd
 from dotenv import load_dotenv
 import os
@@ -8,10 +10,10 @@ load_dotenv(override=True)  # looks for .env in the current directory
 #load environment variables
 database = os.getenv("DATA_ARCHIVE")
 base_url = os.getenv("BASE_URL")
-
+auth = os.getenv("AUTH")
 #connect to the PI Web API
 # Example usage
-conn  = PIWebAPI(base_url=base_url, auth="Kerberos", verify=False)
+conn  = PIWebAPI(base_url=base_url, auth=auth, verify=False)
 
 #read taglist from a CSV file
 taglist = pd.read_csv("taglist.csv")
@@ -28,7 +30,12 @@ starttime = "T-1d"
 endtime = "T"
 
 
+#pull summary data from the server
+data_summary = conn.summary_data(dataserver=database, taglist=taglist, starttime=starttime, endtime=endtime, summaryDuration="1h", summaryType="Average")
 
-#pull compressed data from the server
-data = conn.compressed_data(dataserver=database, taglist=taglist, starttime=starttime, endtime=endtime)
-print(data.describe())
+#pull summary data from the server
+data_compressed = conn.compressed_data(dataserver=database, taglist=taglist, starttime=starttime, endtime=endtime)
+
+#pull interpolated data from the server
+data_interpolated = conn.interpolated_data(dataserver=database, taglist=taglist, starttime=starttime, endtime=endtime, interval='5m')
+
